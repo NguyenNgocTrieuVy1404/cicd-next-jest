b05a92396bf96196dd69a07df79355b2220b86ca
0925225363Vy <nguyenngoctrieuvy.k55ltk@gmail.com>
2026-01-22 19:37:56 +0700

    Initial commit: Next.js with Jest example

---
+++ app/page.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export const metadata = {
+  title: "Welcome to Next.js!",
+  description: "Get started by editing app/page.tsx",
+};
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing <code className={styles.code}>app/page.tsx</code>
+        </p>
+
+        <div className={styles.grid}>
+          <Link href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </Link>
+
+          <Link href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </Link>
+
+          <Link
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </Link>
+
+          <Link href="https://vercel.com/new" className={styles.card}>
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </Link>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <span className={styles.logo}>
+            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  );
+}
